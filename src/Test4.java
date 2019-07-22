654b5f56a157c16ceaf7d73668bd17aaac08f135
fengli <li.feng@weimob.com>
2019-07-22 11:39:14 +0800

    测试2

--- src/Test4.java
+++ src/Test4.java
@@ -1,10 +1,14 @@
 public class Test4 {
 
-    public static void dod(){
+    public static void dod() {
         System.out.println("niidjci.........");
     }
 
-    public static void sds(){
+    public static void sds() {
         System.out.println("hbjknjk.................");
+    }
+
+    public static void sdff() {
+        System.out.println("dwfcewcmwifje");
     }
 }
