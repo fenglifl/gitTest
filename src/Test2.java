0e683910a8599f89723580688a655aee4f4fd862
fengli <li.feng@weimob.com>
2019-07-22 11:01:59 +0800

    测试2

--- src/Test2.java
+++ src/Test2.java
@@ -1,6 +1,6 @@
 public class Test2 {
     //测试git2
     public static void main(String[] args) {
-        System.out.println("测试2..........");
+        System.out.println("测试234343..........");
     }
 }
