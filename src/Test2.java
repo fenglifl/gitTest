51b04e80c7252f23168ba3b775c02e6d4375a23b
fengli <li.feng@weimob.com>
2019-07-22 11:06:06 +0800

    测试2

--- src/Test2.java
+++ src/Test2.java
@@ -3,4 +3,12 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("测试234343..........");
     }
+
+    public static void hello(){
+        System.out.println("aaaaaaaaaaaa");
+    }
+
+    public static void say(){
+        System.out.println("你好.........");
+    }
 }
