ef228dfd1fdce5034dc48d16b11dc9bed286ed1a
fengli <li.feng@weimob.com>
2019-07-25 16:06:22 +0800

    测试修改Test3

--- src/Test3.java
+++ src/Test3.java
@@ -2,4 +2,10 @@ public class Test3 {
     public static void main(String[] args) {
         System.out.println("测试3..........");
     }
+    public void wfwe(){
+        System.out.println("jeiofjreoksjouc");
+    }
+    public void swfg(){
+        System.out.println("jidoxnhbjkoxnhnhfjjjjjjjjjjjjjjjjjj");
+    }
 }
