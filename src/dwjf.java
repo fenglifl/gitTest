113d899210be1e4b0ea01de3e607cd37314da2fd
li.feng <li.feng@weimob.com>
2019-07-31 18:54:16 +0800

    fd

--- src/dwjf.java
+++ src/dwjf.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class dwjf {
+    public void dwd(){
+        System.out.println("dffd");
+        System.out.println("dwszxcwac");
+        System.out.println("dew");
+    }
 }
