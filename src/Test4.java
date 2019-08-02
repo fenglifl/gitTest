6fde646873871ba1c851db5d0eb8dc9a34b0b528
li.feng <li.feng@weimob.com>
2019-08-02 10:08:17 +0800

    测试4

--- src/Test4.java
+++ src/Test4.java
@@ -1,44 +1,9 @@
 public class Test4 {
 
-    public static void dod() {
-        System.out.println("niidjci.........");
-    }
-
-    public static void sds() {
-        System.out.println("hbjknjk.................");
-    }
-
     public static void sdff() {
         System.out.println("dwfcewcmwifje");
         int i;
         i=true?1:2;
         TestEnum testEnum[]=TestEnum.values();
-    }
-    public void sd(){
-        System.out.println("sdsdsdsdsdsds.......");
-    }
-    public void test1(){
-        System.out.println("dkfidjo");
-    }
-    public void djoij(){
-        System.out.println("dfjofj");
-    }
-    public void ddd(){
-        System.out.println("jdeiowfjowei/...");
-    }
-    public void dsfef(){
-        System.out.println("jiewjfw");
-    }
-    public void sjwdhsu(){
-        System.out.println("测试一........");
-    }
-    public void test2(){
-        System.out.println("测试二........");
-    }
-    public void test3(){
-        System.out.println("测试三.........");
-    }
-    public void diojeif(){
-        System.out.println("xjwdifji");
     }
 }
